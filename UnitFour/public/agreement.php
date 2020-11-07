3cef7eb44b008ef6d633fd3013a3671690c1299a
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2020-11-07 19:18:39 +0000

    updated files following help from Shirley's repo as could not understand why not working.

---
+++ UnitFour/public/agreement.php
@@ -0,0 +1,24 @@
+<?php
+include_once 'header.php';
+?>
+<body class = "bg-info">
+<div class = "container-fluid col-md-10 offset-md-1">
+    <div class = "row">
+        <div class = "card mt-3 px-2 py-2">
+            <h1>Stationary Application: Landing page</h1>
+            <p>
+                <?php
+                echo "Welcome. You have selected: ".$_GET["Agreement"]." : for the agreement"
+                ?>
+            </p>
+
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php
+include_once 'footer.php';
+?>
--- UnitFour/public/index.php
+++ UnitFour/public/index.php
@@ -6,19 +6,30 @@ include_once 'header.php';
     <div class = "container-fluid col-md-10 offset-md-1">
         <div class = "row">
             <div class = "card mt-3 px-2 py-2">
-                <h1 text align ="center">Stationary Application</h1>
+                <h1 text-align ="center">Stationary Application</h1>
                 <p><br>Welcome to this fictional stationary application page created
                     for COMP2001 Example. To continue please accept our terms and
                     conditions.</p>
+                <form action="agreement.php" method="GET">
+                    <div class = "form-check">
+                        <input type ='radio' name = 'Agreement' value='true' id='Agree' class="form-check-input"/>
+                        <label class="form-check-label" for="Agree">I Agree</label><br>
+                    </div>
+
+                    <div class = "form-check">
+                        <input type ='radio' name = 'Agreement' value='false' id='Disagree' class="form-check-input"/>
+                        <label class="form-check-label" for="Disagree">I Disagree</label><br>
+                    </div>
+
+                    <div class = form-group>
+                        <input type="submit" value = "Submit" id="Submit" class="btn-primary mb-2">
+                    </div>
+
+                </form>
             </div>
         </div>
     </div>
-    <h1 text align ="center">Stationary Application</h1>
-    <p><br>Welcome to this fictional stationary application page created
-    for COMP2001 Example. To continue please accept our terms and
-    conditions.</p>
-    <h2>Test</h2>
-    <h3>More testing</h3>
+
 
 
 
