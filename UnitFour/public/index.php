ee64bee81a26d828f458c278aeb4457416c4e759
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2020-11-07 14:49:31 +0000

    Add files via upload

---
+++ UnitFour/public/index.php
@@ -0,0 +1,28 @@
+<?php
+include_once 'header.php';
+?>
+
+    <body class = "bg-info">
+    <div class = "container-fluid col-md-10 offset-md-1">
+        <div class = "row">
+            <div class = "card mt-3 px-2 py-2">
+                <h1 text align ="center">Stationary Application</h1>
+                <p><br>Welcome to this fictional stationary application page created
+                    for COMP2001 Example. To continue please accept our terms and
+                    conditions.</p>
+            </div>
+        </div>
+    </div>
+    <h1 text align ="center">Stationary Application</h1>
+    <p><br>Welcome to this fictional stationary application page created
+    for COMP2001 Example. To continue please accept our terms and
+    conditions.</p>
+    <h2>Test</h2>
+
+
+
+<?php
+include_once 'footer.php';
+?>
+
+
